153cee5debe2e0cc17b34ace134994d2d18ca3f7
cy.lee <cy.lee@sk.com>
2019-01-28 15:19:45 +0900

    app.js changed

--- be/app.js
+++ be/app.js
@@ -18,18 +18,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // windodws 와 linux 계열간에 경로 호환되게 해줌.
 
+// 아래는 순서대로 처리됨.
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/set', setRouter);
 
+// 여기까지 경로 없으면 아래로 내려가서 에러 처리
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next) { // next는 다음으로 보내줌.
   next(createError(404));
 });
 
-// error handler
+// error handler 없는 페이지 처리
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
