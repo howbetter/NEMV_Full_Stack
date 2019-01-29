38ae01840a414b61d562ea668bf4c57af3e01413
cy.lee <cy.lee@sk.com>
2019-01-29 19:25:26 +0900

    vuetify ui components test

--- fe/src/views/Help.vue
+++ fe/src/views/Help.vue
@@ -1,10 +1,32 @@
 <template>
   <div class="about">
     <h1> {{title}} </h1>
-    <button @click="test"> test </button>
-    <v-btn :color="btnColor" @click="test"> test </v-btn>
+    <v-btn :color="btnColor" @click="test"> create </v-btn>
+    <v-btn color="primary" @click="rm"> remove </v-btn>
     <v-chip close color="primary"> example chip </v-chip>
-    <v-chip v-for="chip in chips" :color="chip.cl"> {{chip.name}} </v-chip>
+    <v-chip v-for="chip in chips" :key="chip.name" :color="chip.cl"> {{chip.name}} </v-chip>
+    <v-layout row wrap>
+    <v-flex xs12 sm6 offset-sm3 v-for="card in cards" :key="card.name">
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">{{card.name}}</h3>
+            <div>{{card.age}}</div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
   </div>
 </template>
 
@@ -13,6 +35,18 @@ export default {
   name: 'help',
   data () {
     return {
+      cards: [
+      /*
+        {
+          name: 'test1',
+          age: 33
+        },
+        {
+          name: 'test2',
+          age: 31
+        }
+      */
+      ],
       title: 'Vuetify.js',
       btnColor: 'success',
       chips: [
@@ -37,9 +71,17 @@ export default {
   },
   methods: {
     test () {
-      this.title = 'test'
+      this.title = 'HELP PAGE'
       this.btnColor = 'warning'
       console.log('test button is clicked')
+      const card = {
+        name: `CY + ${Math.random()}`
+      }
+      card.age = Math.random()
+      this.cards.push(card)
+    },
+    rm () {
+      this.cards.shift()
     }
   }
 }
