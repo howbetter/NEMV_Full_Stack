eff9b20f66c5e8818d87f48829af780cb7980a7c
cy.lee <cy.lee@sk.com>
2019-01-31 11:30:51 +0900

    RESTful api 추가

--- be/routes/api/user/index.js
+++ be/routes/api/user/index.js
@@ -2,29 +2,49 @@ var express = require('express');
 var router = express.Router();
 var createError = require('http-errors');
 
+
+const us = [
+  {
+    name: "김씨",
+    age: 22
+  },
+  {
+    name: "이씨",
+    age: 33
+  }
+];
+
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
-  const us = [
-    {
-      name: "김씨",
-      age: 22
-    },
-    {
-      name: "이씨",
-      age: 33
-    }
-  ]
-  res.send({
-    users: us
-  })
+  console.log(req.query)
+  console.log(req.body)
+  res.send({ users: us })
 });
+
+router.post('/', (req, res, next) => {
+  console.log(req.query)
+  console.log(req.body)
+  res.send({ success: true, msg: 'post ok' })
+})
+
+router.put('/', (req, res, next) => {
+  console.log(req.query)
+  console.log(req.body)
+  res.send({ success: true, msg: 'put ok' })
+})
+
+router.delete('/', (req, res, next) => {
+  console.log(req.query)
+  console.log(req.body)
+  res.send({ success: true, msg: 'delete ok' })
+})
 
 
 
 router.all('*', function(req, res, next) {
   //res.send('this is api page');
-  next(createError(404, 'there is no api like that'));
+  next(createError(404, 'there is no api like that'))
 });
 
 module.exports = router;
--- fe/src/views/user.vue
+++ fe/src/views/user.vue
@@ -1,24 +1,67 @@
 <template>
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
-      <v-flex xs12 sm6 md4 v-for="u in users">
-        <v-chip close>{{u.name}}</v-chip>
+      <v-flex xs12 sm3>
         <v-card>
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-            aspect-ratio="2.75"
-          ></v-img>
-
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">{{u.name}}</h3>
-              <div>{{u.age}}</div>
+              <h3 class="headline mb-0">GET</h3>
             </div>
           </v-card-title>
-
+          <v-card-text>
+            <v-textarea v-model="getMd">
+            </v-textarea>
+          </v-card-text>
           <v-card-actions>
-            <v-btn flat color="orange">Share</v-btn>
-            <v-btn flat color="orange">Explore</v-btn>
+            <v-btn flat color="orange" @click="getReq">SUBMIT</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm3>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">POST</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <v-textarea v-model="postMd">
+            </v-textarea>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn flat color="orange" @click="postReq">SUBMIT</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm3>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">PUT</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <v-textarea v-model="putMd">
+            </v-textarea>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn flat color="orange" @click="putReq">SUBMIT</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm3>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">DELETE</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <v-textarea v-model="delMd">
+            </v-textarea>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn flat color="orange" @click="delReq">SUBMIT</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -31,19 +74,66 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      users: []
+      users: [],
+      getMd: '',
+      postMd: '',
+      putMd: '',
+      delMd: ''
     }
   },
   mounted () {
-    axios.get('http://localhost:3000/api/user')
-    .then((r) => {
-      this.users = r.data.users
-      console.log(r)
-    })
-    .catch((e) => {
-      console.error(e.message)
-    })
-    console.log('sss')
+  },
+  methods: {
+    // 조회용도
+    getReq () {
+      axios.get('http://localhost:3000/api/user', {
+        user: 'getMan'
+      })
+        .then((r) => {
+          this.getMd = JSON.stringify(r.data)
+          console.log(r)
+        })
+        .catch((e) => {
+          console.error(e.message)
+        })
+    },
+    // 입력 용도
+    postReq () {
+      axios.post('http://localhost:3000/api/user', {
+        user: 'postMan'
+      })
+        .then((r) => {
+          this.postMd = JSON.stringify(r.data)
+          console.log(r)
+        })
+        .catch((e) => {
+          console.error(e.message)
+        })
+    },
+    putReq () {
+      axios.put('http://localhost:3000/api/user', {
+        user: 'putMan'
+      })
+        .then((r) => {
+          this.putMd = JSON.stringify(r.data.msg)
+          console.log(r)
+        })
+        .catch((e) => {
+          console.error(e.message)
+        })
+    },
+    delReq () {
+      axios.delete('http://localhost:3000/api/user', {
+        user: 'delMan'
+      })
+        .then((r) => {
+          this.delMd = JSON.stringify(r.data)
+          console.log(r)
+        })
+        .catch((e) => {
+          console.error(e.message)
+        })
+    }
   }
 }
 </script>
