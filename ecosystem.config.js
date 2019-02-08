4c1d2b0abe59d165ba2de4186c3b3164fc841cdd
cy.lee <cy.lee@sk.com>
2019-02-08 14:18:35 +0900

    pm2 settings

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps : [{
+    name: 'nemv',
+    // script: 'app.js',
+    script: './be/bin/www',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // args: 'one two',
+    // instances: 1,
+    // autorestart: true,
+    // watch: false,  // 코드 변화 확인 후 재시작
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3000
+    },
+    env_prod: {
+      NODE_ENV: 'production',
+      PORT: 80
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
